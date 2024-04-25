364df2c79c9a00a9b259a5268c7b5b395b8e3a83
ruppinCgroup58 <cgroup58ruppin@gmail.com>
2024-04-25 20:12:10 +0300

    pt

---
+++ MEDIQUICK/MEDIQUICK/Controllers/PracticesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MEDIQUICK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PracticesController : ControllerBase
+    {
+        // GET: api/<PracticesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PracticesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PracticesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PracticesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PracticesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
