593959c7b921a30b577d18a3b142ff18db6179cd
maria <maria98vardanian@gmail.com>
2024-04-25 19:48:05 +0300

    class

--- MEDIQUICK/MEDIQUICK/BL/Subject.cs
+++ MEDIQUICK/MEDIQUICK/BL/Subject.cs
@@ -2,5 +2,16 @@
 {
     public class Subject
     {
+        string subjectName;
+        List<string> keyWords;
+
+        public Subject(string subjectName, List<string> keyWords)
+        {
+            SubjectName = subjectName;
+            KeyWords = keyWords;
+        }
+
+        public string SubjectName { get => subjectName; set => subjectName = value; }
+        public List<string> KeyWords { get => keyWords; set => keyWords = value; }
     }
 }
--- MEDIQUICK/MEDIQUICK/BL/Test.cs
+++ MEDIQUICK/MEDIQUICK/BL/Test.cs
@@ -2,5 +2,16 @@
 {
     public class Test
     {
+        int grade;
+        DateTime duration;
+
+        public Test(int grade, DateTime duration)
+        {
+            Grade = grade;
+            Duration = duration;
+        }
+
+        public int Grade { get => grade; set => grade = value; }
+        public DateTime Duration { get => duration; set => duration = value; }
     }
 }
