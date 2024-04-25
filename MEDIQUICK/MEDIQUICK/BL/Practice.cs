6e973d0624252f6d4b837dfdb8ff1c142cd32131
ruppinCgroup58 <cgroup58ruppin@gmail.com>
2024-04-25 20:05:31 +0300

    practice, test

--- MEDIQUICK/MEDIQUICK/BL/Practice.cs
+++ MEDIQUICK/MEDIQUICK/BL/Practice.cs
@@ -2,5 +2,16 @@
 {
     public class Practice
     {
+        string practiceSerialNuber;
+        List<Question> questionsList;
+
+        public Practice(string practiceSerialNuber, List<Question> questionsList)
+        {
+            PracticeSerialNuber = practiceSerialNuber;
+            QuestionsList = questionsList;
+        }
+
+        public string PracticeSerialNuber { get => practiceSerialNuber; set => practiceSerialNuber = value; }
+        public List<Question> QuestionsList { get => questionsList; set => questionsList = value; }
     }
 }
--- MEDIQUICK/MEDIQUICK/BL/Test.cs
+++ MEDIQUICK/MEDIQUICK/BL/Test.cs
@@ -4,14 +4,17 @@
     {
         int grade;
         DateTime duration;
+        List<Question> questionsList;
 
-        public Test(int grade, DateTime duration)
+        public Test(int grade, DateTime duration, List<Question> questionsList)
         {
             Grade = grade;
             Duration = duration;
+            QuestionsList = questionsList;
         }
 
         public int Grade { get => grade; set => grade = value; }
         public DateTime Duration { get => duration; set => duration = value; }
+        public List<Question> QuestionsList { get => questionsList; set => questionsList = value; }
     }
 }
