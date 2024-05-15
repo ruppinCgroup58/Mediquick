443c8724f499ed4a49fe5daa2cc7464cf2b8e0b6
ruppinCgroup58 <cgroup58ruppin@gmail.com>
2024-05-15 20:39:26 +0300

    add ReadQuestions

--- MEDIQUICK/MEDIQUICK/BL/Gemini.cs
+++ MEDIQUICK/MEDIQUICK/BL/Gemini.cs
@@ -2,20 +2,14 @@
 using Google.Api.Gax.Grpc;
 using Google.Cloud.AIPlatform.V1;
 using System.Text;
-using Google.Api.Gax.Grpc;
-using System.Threading.Tasks;
-//using static Google.Cloud.AIPlatform.V1.PredictionService;
-
-using System.Threading.Tasks; //Used for delaying
-
 public class Gemini
 {
-    public async Task<string> GenerateContent(string fileContent)
+    public async Task<string> GenerateContent(string promptToGemini)
     {
-        string projectId = "crested-plexus-422813-c6";  //defined
-        string location = "me-west1";                   //defined
-        string publisher = "google";
-        string model = "gemini-1.5-pro-preview-0409";   //defined
+        string projectId = "crested-plexus-422813-c6";  //modified
+        string location = "me-west1";                   //modified
+        string publisher = "google";                      
+        string model = "gemini-1.5-pro-preview-0409";   //modified
 
         // Create client
         var predictionServiceClient = new PredictionServiceClientBuilder
@@ -41,57 +35,22 @@ public class Gemini
                     Role = "USER",
                     Parts =
                     {
-                        new Part { Text = promptContent.promptText },
-
-                        //new Part { FileData = new() { MimeType = "image/png", FileUri = "gs://generativeai-downloads/images/scones.jpg" } }
+                        new Part { Text = promptContent.promptText }
                     }
-
                 }
             }
-
-            
         };
 
-        var myResponse = "";
-
-        // Make the request, returning a streaming response
-        //using PredictionServiceClient.StreamGenerateContentStream response = predictionServiceClient.StreamGenerateContent(generateContentRequest);
-
-        StringBuilder fullText = new();
-        // Read streaming responses from server until complete
-        //AsyncResponseStream<GenerateContentResponse> responseStream = response.GetResponseStream();
-        // System.Threading.Thread.Sleep(10000);
-
-        //int counter = 0;
         try
         {
+            //Getting the response from google API asynchronously
             GenerateContentResponse response = await predictionServiceClient.GenerateContentAsync(generateContentRequest);
-
             string responseText = response.Candidates[0].Content.Parts[0].Text;
-            Console.WriteLine(responseText);
-
             return responseText;
-
-
-            //while (await responseStream.MoveNextAsync())
-            //{
-            //    GenerateContentResponse responseItem = responseStream.Current;
-            //    await Console.Out.WriteLineAsync(responseItem?.Candidates[0].Content.Parts[0].Text);
-            //    myResponse += responseItem?.Candidates[0].Content.Parts[0].Text;
-            //    System.Threading.Thread.Sleep(1000);
-            //}
         }
         catch (Exception e)
         {
             return e.Message;
         }
-        finally
-        {
-            await Console.Out.WriteLineAsync(myResponse);
-
-        }
-
-        await Console.Out.WriteLineAsync(myResponse);
-        return fullText.ToString();
     }
 }
