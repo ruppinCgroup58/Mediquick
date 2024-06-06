0f71eea96e4711732a742fe9d71c32564067e01a
ruppinCgroup58 <cgroup58ruppin@gmail.com>
2024-06-06 16:36:17 +0300

    topic

--- MEDIQUICK/MEDIQUICK/DBServices/DBServices.cs
+++ MEDIQUICK/MEDIQUICK/DBServices/DBServices.cs
@@ -228,10 +228,12 @@ public class DBServices
                 q.WrongAnswer2 = dataReader["wrongAnswer2"].ToString();
                 q.WrongAnswer3 = dataReader["wrongAnswer3"].ToString();
                 q.Explanation = dataReader["explanation"].ToString();
+                q.Topic = dataReader["topicName"].ToString();
                 q.Status = Convert.ToInt32(dataReader["status"]);
                 q.Creator = dataReader["creatorID"].ToString();
                 q.TotalAnswers = Convert.ToInt32(dataReader["totalAnswers"]);
                 q.TotalCorrectAnswers = Convert.ToInt32(dataReader["correctAnswers"]);
+
                 qList.Add(q); 
             }
             return qList;
