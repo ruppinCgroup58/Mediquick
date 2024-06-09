5a116f8193563d8f1f5c819197b53bb6463e662e
maria <maria98vardanian@gmail.com>
2024-06-09 19:11:51 +0300

    ;

--- mediquick-client/scripts/admin.js
+++ mediquick-client/scripts/admin.js
@@ -163,59 +163,59 @@ function UpdateUserDetailsFormSubmit() {
         phoneNumber: $("#phone-number").val(),
     }
 
-    ajaxCall("Put", apiUpdateUser, JSON.stringify(updateUserDetails), suPostSCB, suPostECB)
+   // ajaxCall("Put", apiUpdateUser, JSON.stringify(updateUserDetails), suPostSCB, suPostECB)
     return false;
 }
 
-function validateForm() {
-    var email = $("#email").val();
-    var firstName = $("#first-name").val();
-    var lastName = $("#last-name").val();
-    var phoneNumber = $("#phone-number").val();
-    var password = $("#password").val();
-    var confirmPassword = $("#confirm-password").val();
+// function validateForm() {
+//     var email = $("#email").val();
+//     var firstName = $("#first-name").val();
+//     var lastName = $("#last-name").val();
+//     var phoneNumber = $("#phone-number").val();
+//     var password = $("#password").val();
+//     var confirmPassword = $("#confirm-password").val();
 
-    // Email validation using regex
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!email.match(emailRegex)) {
-        alert("Invalid email address. Example: ex@123.com");
-        return false;
-    }
+//     // Email validation using regex
+//     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (!email.match(emailRegex)) {
+//         alert("Invalid email address. Example: ex@123.com");
+//         return false;
+//     }
 
-    // First Name and Last Name validation using regex
-    var nameRegex = /^[A-Za-z]+$/;
-    if (!firstName.match(nameRegex)) {
-        alert("Invalid First Name. Name should contain only letters");
-        return false;
-    }
-    if (!lastName.match(nameRegex)) {
-        alert("Invalid Last Name. Name should contain only letters");
-        return false;
-    }
+//     // First Name and Last Name validation using regex
+//     var nameRegex = /^[A-Za-z]+$/;
+//     if (!firstName.match(nameRegex)) {
+//         alert("Invalid First Name. Name should contain only letters");
+//         return false;
+//     }
+//     if (!lastName.match(nameRegex)) {
+//         alert("Invalid Last Name. Name should contain only letters");
+//         return false;
+//     }
 
-    // Phone Number validation using regex
-    var phoneRegex = /^\d{10}$/; // Assuming a 10-digit phone number
-    if (!phoneNumber.match(phoneRegex)) {
-        alert("Invalid phone number. Number shuld contain 10 digits only");
-        return false;
-    }
+//     // Phone Number validation using regex
+//     var phoneRegex = /^\d{10}$/; // Assuming a 10-digit phone number
+//     if (!phoneNumber.match(phoneRegex)) {
+//         alert("Invalid phone number. Number shuld contain 10 digits only");
+//         return false;
+//     }
 
-    // Password validation using regex
-    // At least 8 characters, at least one uppercase letter, one lowercase letter, one number, and one special character
-    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
-    if (!password.match(passwordRegex)) {
-        alert("Invalid password. Password should cotain at least 1 capital latter, 1 regular latter, 1 number, 1 symbol and atleast 8 characters");
-        return false;
-    }
+//     // Password validation using regex
+//     // At least 8 characters, at least one uppercase letter, one lowercase letter, one number, and one special character
+//     var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
+//     if (!password.match(passwordRegex)) {
+//         alert("Invalid password. Password should cotain at least 1 capital latter, 1 regular latter, 1 number, 1 symbol and atleast 8 characters");
+//         return false;
+//     }
 
-    // Confirm Password validation
-    if (password !== confirmPassword) {
-        alert("Passwords do not match");
-        return false;
-    }
+//     // Confirm Password validation
+//     if (password !== confirmPassword) {
+//         alert("Passwords do not match");
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
 
 
