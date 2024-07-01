cc11ac4f0af0fc2b91effd5ff2d1c471f1cea16d
natnahshonov <159234420+natnahshonov@users.noreply.github.com>
2024-07-01 16:35:43 +0300

    פורום

---
+++ MEDIQUICK/MEDIQUICK/BL/Comment.cs
@@ -0,0 +1,41 @@
+﻿namespace MEDIQUICK.BL
+{
+    public class Comment
+    {
+        int commentId;
+        int issueId;
+        int userId;
+        string content;
+        DateTime createdAt;
+        DBServices dbs = new DBServices();
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(int commentId, int issueId, int userId, string content, DateTime createdAt)
+        {
+            this.commentId = commentId;
+            this.issueId = issueId;
+            this.userId = userId;
+            this.content = content;
+            this.createdAt = createdAt;
+        }
+
+        public int CommentId { get => commentId; set => commentId = value; }
+        public int IssueId { get => issueId; set => issueId = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public string Content { get => content; set => content = value; }
+        public DateTime CreatedAt { get => createdAt; set => createdAt = value; }
+
+        public int Insert()
+        {
+            return dbs.InsertComment(this);
+        }
+
+
+
+
+    }
+}
---
+++ MEDIQUICK/MEDIQUICK/BL/Issue.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Net;
+
+namespace MEDIQUICK.BL
+{
+    public class Issue
+    {
+        int issueId;
+        int topicId;
+        int userId;
+        string title;
+        string content;
+        DateTime createdAt;
+        DBServices dbs = new DBServices();
+        public Issue()
+        {
+
+        }
+        public Issue(int issueId, int topicId, int userId, string title, string content, DateTime createdAt)
+        {
+            this.issueId = issueId;
+            this.topicId = topicId;
+            this.userId = userId;
+            this.title = title;
+            this.content = content;
+            this.createdAt = createdAt;
+        }
+
+        public int IssueId { get => issueId; set => issueId = value; }
+        public int TopicId { get => topicId; set => topicId = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public string Title { get => title; set => title = value; }
+        public string Content { get => content; set => content = value; }
+        public DateTime CreatedAt { get => createdAt; set => createdAt = value; }
+
+
+        public int Insert()
+        {
+            return dbs.InsertIssue(this);
+        }
+
+    }
+}
