ba4a1d7de397e1070f762a600daa15de87f61d87
ruppinCgroup58 <cgroup58ruppin@gmail.com>
2024-08-01 11:28:53 +0300

    1

--- mediquick-client/scripts/test-script.js
+++ mediquick-client/scripts/test-script.js
@@ -1,13 +1,40 @@
 let testAPI = localHostAPI + "api/Tests"
 let userConnected = sessionStorage.getItem("id");
+let testId;
 
 function startTest() {
     let startTestAPI = testAPI + "/userId/" + userConnected;
-    ajaxCall("POST", startTestAPI, userConnected, startTestSCB, startTestECB);
+    //ajaxCall("POST", startTestAPI, userConnected, startTestSCB, startTestECB);
+    var preTest = document.querySelector('.pre-test');
+    var testContent = document.querySelector('.test-content');
+
+    // הסתרת pre-test עם אנימציה
+    preTest.style.opacity = '0';
+    preTest.style.maxHeight = '0';
+
+    // הצגת test-content עם אנימציה לאחר זמן קצר
+    setTimeout(function() {
+        preTest.style.display = 'none';
+        testContent.style.display = 'block';
+        setTimeout(function() {
+            testContent.style.opacity = '1';
+            testContent.style.maxHeight = '1000px'; // הגדר גובה מלא שמתאים לתוכן שלך
+        }, 50);
+    }, 500);
+    
 }
 
-function startTestSCB(questionsList) {
-
+function startTestSCB(testObject) {
+    testId = testObject.testId;
+    let testContentDiv = document.querySelector(".test-content");
+    let str = "";
+    let i = 1;
+    str += `<div class="grid-list">`
+    while (i <= 30) {
+        str += `<div data-number="${i}" class="grid-item">${i}</div>`
+    }
+    str += `</div>`;
+    testContentDiv.innerHTML += str;
 }
 
 function startTestECB(err) {
